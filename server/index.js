d851f56f3ba8a63eff5d7310fa692f3eb487a7c3
Hack Reactor Student <student@hackreactor.com>
2019-07-03 20:23:53 -0700

    image scraper module implemented.  AWS RDS function, Google Cloud storage functional and now has the option to be referenced.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const parser = require('body-parser');
-const connection = require('../database/index.js');
+// const connection = require('../database');
 const port = 3000;
+// const scraper = require('../scraped-imgs/index.js');
 
 
 app.listen(port, ()=> console.log(`Listening to port ${port}`))
