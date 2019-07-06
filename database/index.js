2265a24d36b4097fdcaa42beb1b37337ce2138f2
Christopher Pham <ciang@Christophers-MacBook-Pro.local>
2019-07-06 15:13:01 -0700

    GCS metadata functions written and data is now retrievable.

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
-// var mysql = require('mysql');
 var Credentials = require('../credentials.js');
 const Sequelize = require('sequelize');
+//                                  db           user       pw             options
 const connection = new Sequelize('fecwestelms', 'admin', Credentials.mypw, {
     host : Credentials.host,
     port: '3306',
@@ -13,24 +13,3 @@ connection
     .catch( (err) => console.error('Not connected to the database', err));
 
 module.exports = connection;
-
-// RAW SQL
-// var connection = mysql.createConnection({
-//   host     : 'admin.cmd4su8z0tlg.us-west-1.rds.amazonaws.com',
-//   user     : 'admin',
-//   password : mypw,
-//   port     : '3306'
-// });
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('Database connection failed: ' + err.stack);
-//     return;
-//   }
-
-//   console.log('Connected to database.');
-// });
-
-// connection.end();
-
-// module.exports = connection;
--- gcloud/index.js
+++ gcloud/index.js
@@ -1,13 +1,54 @@
 const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
+
 async function listBuckets() {
 
-
+	//bucket.file(fileName) will return the file
 	const [buckets] = await storage.getBuckets();
 	console.log('Buckets:')
 	buckets.forEach(bucket => {
-		console.log(bucket.name);
+		if (bucket.name === 'afucktonofchairs') {
+			listFiles(bucket.name, (results) => {
+				console.log('Files in Bucket:', (results));
+			});
+		}
 	})
 }
 
+async function listFiles(bucketName, callback) {
+
+
+	console.log('bucketName:', bucketName)
+	var results = [];
+	// Lists files in the bucket
+	const [files] = await storage.bucket(bucketName).getFiles();
+
+	console.log('Files:');
+	files.forEach((file, i) => {
+		var storageEndPoint = 'https://storage.cloud.google.com/afucktonofchairs/';
+		if (file.name.endsWith('.jpeg') || file.name.endsWith('.jpg')) {
+			storageEndPoint += file.name;
+			results.push(storageEndPoint);
+			if (i === files.length-1) {
+				callback(results);
+			}
+		}
+	});
+	// [END storage_list_files]
+}
+
 listBuckets();
+
+/*
+Reflection:
+
+What is the purpose of using gcloud storage functions in context of this app?
+
+- The primary purpose is being able to grab metadata.  Here we can create an array of current URLs in our chairs bucket.
+
+How will this aid in our search queries using AWS RDS?
+
+ - We can update the database for any plausible new image files.  However, the problem is that we grab only the URL, and thus needs more exploration.
+ - Otherwise, we reference AWS RDS records for URL lookup to serve via express.Í
+
+*/
