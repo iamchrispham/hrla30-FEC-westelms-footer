19b9e4353ff52218a4d225a9b22a8aa3fbe7bc66
Christopher Pham <chrismpham@gmail.com>
2019-07-07 18:04:16 -0700

    Images referenced from AWS RDS and rednering successfully via Axios on Client-side.  CSS needs ironing out.

--- client/src/components/footer/footerlink.jsx
+++ client/src/components/footer/footerlink.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FooterLinks = (props) => {
-  console.log('FooterLinks(props):', props);
+  // console.log('FooterLinks(props):', props);
   if (props.sublinks.length === 0) { // if we've reached the furthest right of the footer
     return (
       <div>
@@ -11,6 +11,9 @@ const FooterLinks = (props) => {
             Implement signup component here
             We can hella nest divs!
             */}
+            {/* <p>*SIGNUP COMPONENT FORM*</p>
+            <p>*PLAUSIBLE SVG COMPONENT*</p>
+            <p>*SOCIAL MEDIA CSS ICONS*</p> */}
           </div>
         </div>
       </div>
--- client/src/components/product-recs/carousel.jsx
+++ client/src/components/product-recs/carousel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -10,12 +11,50 @@ class Carousel extends React.Component {
       onClickRightArrow: false,
       images: []
     }
+    this.getAllImagesAPI = this.getAllImagesAPI.bind(this);
+    this.renderImages = this.renderImages.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('Carousel component mounting...');
+    this.getAllImagesAPI();
+  }
+
+  getAllImagesAPI() {
+    axios.get('/products-cp/')
+      .then(({ data }) => { // can deconstruct object just for the data property
+        // var { data } = resp;
+        this.setState({
+          images: data
+        }, () => console.log('New Image Data', this.state.images))
+
+      })
+      .catch((err) => {
+        console.log('Error grabbing images.', err);
+      })
+  }
+// style="width:225px;height:247px"/
+  renderImages() {
+    return (
+      <ul className="scroller">
+        {
+          this.state.images.map((image, i) => {
+            console.log(`ImgData(${i}): ${image['url']}`)
+            return (
+              <li className={`scroller-id ${image['id']}`}>
+              <img src={image['url']} alt={image['name']} width="225px" height="247px"/>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
   }
 
   render() {
     return (
       <div className="carousel-cp" id="carousel-cp">
-        This is a carousel div.
+        { this.state.images ? this.renderImages() : null}
     </div>
 
     )
