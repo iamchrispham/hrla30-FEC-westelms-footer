d91c2395ae182593ee1595d11f3bfd12c1cf3944
Christopher Pham <chrismpham@gmail.com>
2019-07-11 06:22:27 -0700

    further CSS improvements

--- client/src/components/product-recs/carousel/section-header.jsx
+++ client/src/components/product-recs/carousel/section-header.jsx
@@ -4,6 +4,7 @@ const SectionHeader = (props) => {
   return (
     <div className="sectionHeader" id="sectionHeader">
       <h2>People Also Browsed</h2>
+      <a>See More Personalized Picks</a>
     </div>
   )
 }
--- client/src/components/product-recs/product-container.jsx
+++ client/src/components/product-recs/product-container.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from './carousel/carousel.jsx';
-import SectionHeader from './section-header.jsx';
+import SectionHeader from './carousel/section-header.jsx';
 
 // ProductContainer both parent of section-header and carousel 
   const ProductContainer = (props) => ( // reminder: use ES6 syntax for stateless components
